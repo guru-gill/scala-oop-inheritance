88b39f2ce651c12055d6a9af7c74671b632b4905
Amandeep Kaur <amanmaan060@gmail.com>
2020-02-01 23:05:30 -0500

    This is simple example for inheritance of class. Simple run Main_object

--- src/main/scala/oop/SquareCuboid.scala
+++ src/main/scala/oop/SquareCuboid.scala
@@ -4,6 +4,8 @@ class SquareCuboid (var side:Int,var hight:Int)extends Square(side) {
 
   override def draw(): String = s"This is the Area of Square Cuboid: ${area()}"
 
+  def volume(): Double = side*side*hight
+
 
 
   override def area(): Double = (super.area()+super.area()*(4*side*hight))
